3eef354b68f5bdf7334c11b1167cd7cf0848063a
Mary Nydegger <116200462+MaryNydegger@users.noreply.github.com>
2023-12-07 12:02:01 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.sidebar.title('Interactive Controls')
-user_input = st.sidebar.slider('Select a number:', 1, 10, 5)
+st.title('Lab 10- Streamlit')
+Apple = pd.read_csv('AppleData.csv')
 
+prices = st.sidebar.slider('Select Price: ', min_value = 0.0, max_value = 20.0, value = (0.0, 20.0))
+genres = Apple['prime_genre'].unique()
+genre = st.sidebar.multiselect('Select Genre: ', genres, default = genres)
 
-data = np.random.randn(100, user_input)
+apple_filtered = Apple[(Apple['prime_genre'].isin(genre)) & (Apple['price'] >= prices[0]) & (Apple['price'] <= prices[1])]
 
-st.write('### Random Data Generated:')
-st.write(pd.DataFrame(data))
+if st.checkbox('View Data'):
+    st.subheader('raw Data')
+    st.write(apple_filtered)
 
-st.write('### Line Chart:')
-plt.figure(figsize=(8, 6))
-plt.plot(np.mean(data, axis=0))
-st.pyplot()
+st.subheader('Top 5 Apps')
+top_five = apple_filtered.sort_values(by = 'rating_count_tot', ascending = False).head(5)
+st.bar_chart(top_five.set_index('track_name')['rating_count_tot'], use_container_width=True)
 
-st.write('This app generates random data based on user input and displays it in a DataFrame. It also plots a line chart of the mean of the generated data.')
+st.write('This app generates data from the Apple Store for the top 5 apps and their ratings')
