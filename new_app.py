98d81e786e8b0c37c69013b3f5308aae7596d09d
Mary Nydegger <116200462+MaryNydegger@users.noreply.github.com>
2023-12-13 00:46:38 -0700

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -2,9 +2,24 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
-In [2]:
+
 st.title('Dynamic Graph Lab 10')
 
 number = st.slider('Select a number ', 1, 10)
 option = st.selectbox('Select an Option ', ['A', 'B', 'C'])
+
+data = pd.DataFrame({
+    'x': np.arange(1, number + 1),
+    'y': np.random.randint(1, 100, size = number)
+})
+
+st.write(f'Displaying data for option {option}:')
+st.write(data)
+In [6]:
+plt.figure(figsize = (8,6))
+plt.bar(data['x'], data['y'])
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Dynamic Bar Chart')
+st.pyplot()
 
